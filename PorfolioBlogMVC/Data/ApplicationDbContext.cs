9246f141e1587fd8b1abb06f2c54d3591648b36b
Gab <gabfer258@gmail.com>
2025-12-09 16:25:16 +0100

    initial project setup with ASP.NET Core MVC, including layout, views, and identity schema

---
+++ PorfolioBlogMVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace PorfolioBlogMVC.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
